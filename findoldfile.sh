3608d0be841b75d0e876cec2fc2c7596755199f5
skye1349 <yee131419@gamil.com>
2023-06-15 18:13:24 +1000

    script to find old file & exec list/delete/rename

---
+++ findoldfile.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Author: Skye
+# Date: 15/06/2023
+# Description: This script will delete files older than 90 days
+# Modified: 15/06/2023
+
+#list old file
+find /home/ubuntu/ps -mtime +90 -exec ls -l {} \;
+#delete old file
+find /home/ubuntu/ps -mtime +90 -exec rm {} \;
+# rename old file
+find /home/ubuntu/ps -mtime +90 -exec mv {} {} .old \;
