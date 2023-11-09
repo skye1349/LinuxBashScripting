c8b7e80150ac235e73753ff7b3ba546ef287bd53
skye1349 <yee131419@gamil.com>
2023-11-09 21:43:33 +1100

    test

--- scripts/encrypt_check.sh
+++ scripts/encrypt_check.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
-for file in "$@"; do
-  if grep -q "ANSIBLE" "$file"; then
-    echo "File $file contains ANSIBLE"
-  else
+
+# Get a list of all staged .yml files
+STAGED_FILES=$(git diff --cached --name-only -- '*.yml')
+
+# Check each staged .yml file for the presence of "ANSIBLE"
+for file in $STAGED_FILES; do
+  if ! grep -q "ANSIBLE" "$file"; then
     echo "Error: Must Encrypt Yml First!"
     exit 1
   fi
 done
+
+# If we reach this point, all files contain "ANSIBLE"
+exit 0
+
