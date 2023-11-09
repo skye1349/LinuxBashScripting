3902475cabc429d0734d38b1a7beb59482744784
skye1349 <yee131419@gamil.com>
2023-11-09 23:12:14 +1100

    test

--- scripts/encrypt_check.sh
+++ scripts/encrypt_check.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 for file in "$@"; do
   if grep -q "\$ANSIBLE_VAULT;1:1;AES256" "$file"; then
-    echo "good to go"
+    echo "good to go$file"
   else
-    echo "Error: You must encrypt YML first!"
+    echo "Error: You must encrypt $file first!"
     exit 1
   fi
 done
